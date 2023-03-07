cd201569b390d80ec04a06e8402b6a5bdfa811a7
meg <65887771+9rotama@users.noreply.github.com>
2023-03-07 20:29:14 +0900

    線が描ける

--- front/app/paint/Canvas.tsx
+++ front/app/paint/Canvas.tsx
@@ -1,27 +1,78 @@
 'use client';
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 export default function Canvas() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const canvasOverlayRef = useRef<HTMLCanvasElement>(null);
-  let ctx: CanvasRenderingContext2D | null | undefined;
-
   const canvasWidth = 720;
   const canvasHeight = 720;
 
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvasOverlayRef = useRef<HTMLCanvasElement>(null);
+
+  const [states, setStates] = useState({
+    isDrawing: false,
+    offsetX: 0,
+    offsetY: 0,
+    startX: 0,
+    startY: 0,
+  });
+
+  useEffect(() => {
+    let canvasRect = canvasRef.current?.getBoundingClientRect();
+
+    if (canvasRect) {
+      setStates({
+        ...states,
+        offsetX: canvasRect.left,
+        offsetY: canvasRect.top,
+      });
+    }
+    drawBg();
+  }, []);
+
   const drawBg = () => {
-    if (ctx != null) {
+    let ctx = canvasRef.current?.getContext('2d');
+    if (ctx) {
       ctx.fillStyle = 'white';
       ctx.fillRect(0, 0, canvasWidth, canvasHeight);
     }
   };
 
-  const handleMouseDown = () => {};
+  const handleMouseDown = (
+    e: React.MouseEvent<HTMLCanvasElement, MouseEvent>,
+  ) => {
+    let ctx = canvasRef.current?.getContext('2d');
 
-  useEffect(() => {
-    ctx = canvasRef.current?.getContext('2d');
-    drawBg();
-  }, []);
+    if (ctx) {
+      setStates({ ...states, isDrawing: true });
+      ctx.beginPath();
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = 1;
+      ctx.lineJoin = ctx.lineCap = 'round';
+      ctx.moveTo(e.clientX - states.offsetX, e.clientY - states.offsetY);
+    }
+  };
+
+  const handleMouseMove = (
+    e: React.MouseEvent<HTMLCanvasElement, MouseEvent>,
+  ) => {
+    let ctx = canvasRef.current?.getContext('2d');
+    if (ctx) {
+      if (states.isDrawing) {
+        ctx.lineTo(e.clientX - states.offsetX, e.clientY - states.offsetY);
+        ctx.stroke();
+      }
+    }
+  };
+
+  const handleMouseUp = (
+    e: React.MouseEvent<HTMLCanvasElement, MouseEvent>,
+  ) => {
+    let ctx = canvasRef.current?.getContext('2d');
+    if (ctx) {
+      ctx.closePath();
+      setStates({ ...states, isDrawing: false });
+    }
+  };
 
   return (
     <div className="bg-gray-200 p-3">
@@ -30,6 +81,9 @@ export default function Canvas() {
         width={canvasWidth.toString()}
         height={canvasHeight.toString()}
         ref={canvasRef}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
       >
         <canvas
           className="absolute top-0 left-0"
