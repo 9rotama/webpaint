85cf092009928176f4e6ecf4266d6009904a8c10
meg <65887771+9rotama@users.noreply.github.com>
2023-03-27 22:34:49 +0900

    データベース設定

--- api/app/main.py
+++ api/app/main.py
@@ -1,7 +1,42 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Work:
+    id: str
+    url: str
+    likes: int
+
+
+class PostParam(BaseModel):
+    artist: str
+    name: str
+    description: str
+    image_data: str
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/discover/{page_num}")
+async def discover(page_num: int):
+    return {"message": page_num}
+
+
+@app.get("/work/{work_id}")
+async def work(work_id: str):
+    return {"message": work_id}
+
+
+@app.post("/post")
+async def post(param: PostParam):
+    return {"message": param}
+
+
+@app.put("/like/{work_id}")
+async def like(work_id: str):
+    return {"message": work_id}
