0c34b16404876a0bd7255e699bd1f2d3b66e95bf
meg <65887771+9rotama@users.noreply.github.com>
2023-04-26 16:35:39 +0900

    イラストデータ一覧をgetするテスト

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,5 +1,13 @@
-import Link from 'next/link';
+'use client';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 export default function Page() {
+  useEffect(() => {
+    const get_url = process.env.NEXT_PUBLIC_API_URL + '/discover/1';
+    axios.get(get_url).then((response) => {
+      console.log('body:', response.data);
+    });
+  }, []);
   return <div className=""></div>;
 }
