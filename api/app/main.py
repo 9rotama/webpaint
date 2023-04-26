212986e9104502cb4a33fb982108ebcd86a0a336
meg <65887771+9rotama@users.noreply.github.com>
2023-04-26 16:15:26 +0900

    CORSの設定

--- api/app/main.py
+++ api/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 import schemas
 import crud
@@ -8,6 +9,14 @@ Base.metadata.create_all(bind=ENGINE)
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
