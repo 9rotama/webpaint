c9adc4df82fedfc1b28c99b68772c782e0420271
meg <65887771+9rotama@users.noreply.github.com>
2023-05-03 01:00:32 +0900

    ナビゲーションバーのスタイル調整

--- front/app/GlobalNav.tsx
+++ front/app/GlobalNav.tsx
@@ -1,10 +1,26 @@
+'use client';
+
 import NavButton from '@/ui/NavButton';
+import { usePathname } from 'next/navigation';
+import { faImage, faPaintbrush } from '@fortawesome/free-solid-svg-icons';
 
 export default function GlobalNav() {
+  const pathname = usePathname();
+
   return (
-    <div>
-      <NavButton href="/" text="home" />
-      <NavButton href="/paint" text="paint" />
+    <div className="m-2 flex justify-center">
+      <NavButton
+        href="/"
+        icon={faImage}
+        text="gallery"
+        isActive={pathname == '/'}
+      />
+      <NavButton
+        href="/paint"
+        icon={faPaintbrush}
+        text="paint"
+        isActive={pathname == '/paint'}
+      />
     </div>
   );
 }
